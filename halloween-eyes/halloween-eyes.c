9ebede4096aea201a7d6966acaf0991bf636f90b
Michael Bemmerl <mail@mx-server.de>
2012-10-18 01:41:14 +0200

    Setup of brightness of the LED implemented

--- halloween-eyes/halloween-eyes.c
+++ halloween-eyes/halloween-eyes.c
@@ -14,19 +14,46 @@
 #error F_CPU not defined!
 #endif
 
+void setup_brightness(void);
+void setup_blinkrate(void);
+
 int main(void)
 {
-	// Pins as output
+	// LED pin as output
 	DDRB = LED;
 
-	// Interner-Pull-Up aktiv
+	// Activate pull-up for button
 	PINB |= BUTTON;
 
-	setup_adc();
-	read_adc();
+	// Initialize ADC & Timer
+	adc_setup();
+	adc_read();
+	pwm_setup();
 
-	while(1)
-	{
-	}
+	setup_brightness();
+	setup_blinkrate();
 }
 
+void setup_brightness(void)
+{
+	// Loop as long as button is not pressed
+	while(PINB & BUTTON)
+	{
+		// Get value from pot
+		uint8_t value = adc_read();
+		pwm_set(value);
+	}
+
+	// Poor man's debouncing
+	_delay_ms(250);
+
+	// wait until button is not pressed anymore
+	while(!(PINB & BUTTON));
+
+	_delay_ms(250);
+}
+
+void setup_blinkrate(void)
+{
+	
+}
