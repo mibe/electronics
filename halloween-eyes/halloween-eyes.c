4d9b56c7bea8d2a2b8bf13781e92e48c212b2092
Michael Bemmerl <mail@mx-server.de>
2012-10-25 01:24:02 +0200

    Two types of blinking (static / random)

--- halloween-eyes/halloween-eyes.c
+++ halloween-eyes/halloween-eyes.c
@@ -1,6 +1,7 @@
 #include <avr/io.h>
 #include <stdlib.h>
 #include <util/delay.h>
+#include <stdbool.h>
 
 #include "adc.h"
 #include "pwm.h"
@@ -15,7 +16,9 @@
 #endif
 
 void setup_brightness(void);
-void blink();
+void blink_mode_switched(void);
+void blink_static(bool);
+void blink_random(bool);
 void wait(uint8_t wait);
 
 int main(void)
@@ -31,13 +34,28 @@ int main(void)
 	adc_read();
 	pwm_setup();
 
+	// Seed the PRNG with value from pot
 	srand(adc_read());
 
 	setup_brightness();
 
+	uint8_t mode = 0;
+
 	while(1)
 	{
-		blink();
+		if (!(PINB & BUTTON))
+		{
+			mode++;
+			blink_mode_switched();
+		}
+
+ 		if (mode == 2)
+ 			mode = 0;
+
+		if (mode == 0)
+			blink_static(false);
+		else
+			blink_random(false);
 	}
 }
 
@@ -60,32 +78,52 @@ void setup_brightness(void)
 	_delay_ms(250);
 }
 
-void blink()
+void blink_static(bool defaultOn)
 {
-// 	pwm_off();
-// 	_delay_ms(5000);
-// 	pwm_on();
-// 	_delay_ms(100);
-// 	pwm_off();
-// 	_delay_ms(200);
-// 	pwm_on();
-// 	_delay_ms(100);
+	defaultOn ? pwm_on() : pwm_off();
 
-// 	pwm_on();
-// 	_delay_ms(5000);
-// 	pwm_off();
-// 	_delay_ms(150);
-// 	pwm_on();
-// 	_delay_ms(400);
-// 	pwm_off();
-// 	_delay_ms(150);
-// 	pwm_on();
+	_delay_ms(5000);
 
-	pwm_on();
+	defaultOn ? pwm_off() : pwm_on();
+
+	_delay_ms(150);
+
+	defaultOn ? pwm_on() : pwm_off();
+
+	_delay_ms(400);
+
+	defaultOn ? pwm_off() : pwm_on();
+
+	_delay_ms(150);
+
+	defaultOn ? pwm_on() : pwm_off();
+}
+
+void blink_random(bool defaultOn)
+{
+	defaultOn ? pwm_on() : pwm_off();
+
 	uint8_t rando = rand();
 	wait(rando);
-	pwm_off();
+
+	defaultOn ? pwm_off() : pwm_on();
+
 	_delay_ms(150);
+}
+
+void blink_mode_switched(void)
+{
+	uint8_t times = 5;
+
+	while(times-- > 0)
+	{
+		pwm_off();
+		_delay_ms(200);
+		pwm_on();
+		_delay_ms(200);
+	}
+	
+	_delay_ms(1000);
 }
 
 void wait(uint8_t wait)
