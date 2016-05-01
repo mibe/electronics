657668108b04bd961b6ac430069a5ebf6508bb04
Michael Bemmerl <mail@mx-server.de>
2016-05-01 23:41:52 +0200

    It's compiling!

--- feuermelder/firmware/main.c
+++ feuermelder/firmware/main.c
@@ -18,83 +18,45 @@
 
 /* ----------------------- hardware I/O abstraction ------------------------ */
 
-/* pin assignments:
-PB0	Key 1
-PB1	Key 2
-PB2	Key 3
-PB3	Key 4
-PB4	Key 5
-PB5 Key 6
-
-PC0	Key 7
-PC1	Key 8
-PC2	Key 9
-PC3	Key 10
-PC4	Key 11
-PC5	Key 12
-
-PD0	USB-
-PD1	debug tx
-PD2	USB+ (int0)
-PD3	Key 13
-PD4	Key 14
-PD5	Key 15
-PD6	Key 16
-PD7	Key 17
-*/
-
-static void hardwareInit(void)
-{
-uchar	i, j;
-
-    PORTB = 0xff;   /* activate all pull-ups */
-    DDRB = 0;       /* all pins input */
-    PORTC = 0xff;   /* activate all pull-ups */
-    DDRC = 0;       /* all pins input */
-    PORTD = 0xfa;   /* 1111 1010 bin: activate pull-ups except on USB lines */
-    DDRD = 0x07;    /* 0000 0111 bin: all pins input except USB (-> USB reset) */
-	j = 0;
-	while(--j){     /* USB Reset by device only required on Watchdog Reset */
-		i = 0;
-		while(--i); /* delay >10ms for USB reset */
-	}
-    DDRD = 0x02;    /* 0000 0010 bin: remove USB reset condition */
-    /* configure timer 0 for a rate of 12M/(1024 * 256) = 45.78 Hz (~22ms) */
-    TCCR0 = 5;      /* timer 0 prescaler: 1024 */
-}
 
 /* ------------------------------------------------------------------------- */
 
 #define NUM_KEYS    17
+
+/* Pin, onto which the switch is wired to.
+ * The pins for the USB D- and D+ line cannot be used here, of course.
+ */
+#define SWITCH_PIN PB4
+
 
 /* The following function returns an index for the first key pressed. It
  * returns 0 if no key is pressed.
  */
 static uchar    keyPressed(void)
 {
-uchar   i, mask, x;
+// uchar   i, mask, x;
 
-    x = PINB;
-    mask = 1;
-    for(i=0;i<6;i++){
-        if((x & mask) == 0)
-            return i + 1;
-        mask <<= 1;
-    }
-    x = PINC;
-    mask = 1;
-    for(i=0;i<6;i++){
-        if((x & mask) == 0)
-            return i + 7;
-        mask <<= 1;
-    }
-    x = PIND;
-    mask = 1 << 3;
-    for(i=0;i<5;i++){
-        if((x & mask) == 0)
-            return i + 13;
-        mask <<= 1;
-    }
+    // x = PINB;
+    // mask = 1;
+    // for(i=0;i<6;i++){
+        // if((x & mask) == 0)
+            // return i + 1;
+        // mask <<= 1;
+    // }
+    // x = PINC;
+    // mask = 1;
+    // for(i=0;i<6;i++){
+        // if((x & mask) == 0)
+            // return i + 7;
+        // mask <<= 1;
+    // }
+    // x = PIND;
+    // mask = 1 << 3;
+    // for(i=0;i<5;i++){
+        // if((x & mask) == 0)
+            // return i + 13;
+        // mask <<= 1;
+    // }
     return 0;
 }
 
@@ -254,7 +216,10 @@ uchar   key, lastKey = 0, keyDidChange = 0;
 uchar   idleCounter = 0;
 
 	wdt_enable(WDTO_2S);
-    hardwareInit();
+    
+	// Activate the pull-up on the switch pin
+	PORTB |= _BV(SWITCH_PIN);
+
 	odDebugInit();
 	usbInit();
 	sei();
