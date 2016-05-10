f638af9661d6873094fffe59d624a2d72963d787
Michael Bemmerl <mail@mx-server.de>
2016-05-10 21:09:49 +0200

    License stuff

--- feuermelder/firmware/main.c
+++ feuermelder/firmware/main.c
@@ -1,11 +1,31 @@
 /* Name: main.c
- * Project: HID-Test
- * Author: Christian Starkjohann
- * Creation Date: 2006-02-02
- * Tabsize: 4
- * Copyright: (c) 2006 by OBJECTIVE DEVELOPMENT Software GmbH
- * License: GNU GPL v2 (see License.txt) or proprietary (CommercialLicense.txt)
- * This Revision: $Id$
+ * 
+ * Part of the "Feuermelder" (German for "fire call point") project, which sends
+ * the hotkey Ctrl+Alt+Delete or Ctrl+Shift+Escape to the USB host. This will
+ * (at least on Windows) open the task manager. Hence the name, "Feuermelder".
+ *
+ * This work is based on the following project:
+ * 		v-usb framework http://www.obdev.at/vusb/
+ * 		Project: "HIDKeys" example
+ * 		Author: Christian Starkjohann
+ * 		Copyright: (c) 2006 by OBJECTIVE DEVELOPMENT Software GmbH
+ *		License: see usbdrv/License.txt (GNU GPL v2, GNU GPL v3 or proprietary)
+ *
+ * Copyright: (C) 2016 by Michael Bemmerl
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
 #include <avr/io.h>
