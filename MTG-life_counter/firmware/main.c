7f122152c6876bc24cb4949ebaee71645149102f
Michael Bemmerl <mail@mx-server.de>
2022-10-25 23:50:55 +0200

    Fixed various stuff:
    - The counting function now works properly (fixed #9)
    - When multiplexing the correct digit is driven
    - More comments here and there

--- MTG-life_counter/firmware/main.c
+++ MTG-life_counter/firmware/main.c
@@ -5,34 +5,51 @@
 #define MAX 99
 #define MIN 0
 
-volatile uint16_t counter = 0;
-volatile uint8_t dummy = 0;
+#define INC_BTN PB1
+#define DEC_BTN PB2
+#define DIGIT1 PB3
+#define DIGIT2 PB4
+
+uint8_t counter = 20;
+volatile uint8_t digit1 = 0;
+volatile uint8_t digit2 = 0;
 
 const uint8_t digits[10] = {63, 6, 91, 79, 102, 109, 125, 7, 127, 111};
 const uint8_t mask = _BV(PB1) | _BV(PB2);
 
 ISR(TIMER0_OVF_vect)
 {
-	if ((PORTB & _BV(PB3)) == _BV(PB3))
+	// Multiplex both 7-segment displays:
+	// Switch active digit, depending on which one is active.
+	if ((PORTB & _BV(DIGIT1)) == _BV(DIGIT1))
 	{
-		PORTB &= ~_BV(PB3);
-		PORTB |= _BV(PB4);
+		// disable second, enable first
+		PORTB &= ~_BV(DIGIT1);
+		PORTB |= _BV(DIGIT2);
+		
+		PORTD = digit1;
 	}
 	else
 	{
-		PORTB |= _BV(PB3);
-		PORTB &= ~_BV(PB4);
+		// disable first, enable second
+		PORTB |= _BV(DIGIT1);
+		PORTB &= ~_BV(DIGIT2);
+		
+		PORTD = digit2;
 	}
 }
 
 void setup(void)
 {
 	// Set both anode drivers of the digits as outputs. Port D is also an output.
-	DDRB |= _BV(PB0) | _BV(PB3) | _BV(PB4);
+	DDRB |= _BV(PB0) | _BV(DIGIT1) | _BV(DIGIT2);
 	DDRD = 0xFF;
 	
-	// Enable pull-ups on PB1 & PB2 for the push buttons.
-	PORTB |= _BV(PB1) | _BV(PB2);
+	// Enable pull-ups on both pins for the push buttons. Also set all Port D pins high (disable display).
+	PORTB |= _BV(INC_BTN) | _BV(DEC_BTN);
+	PORTD = 0xFF;
+	
+	update_display();
 	
 	// Timer0 used for multiplexing both 7-segment displays
 	// Prescaler of 64, overflow interrupt enabled
@@ -49,6 +66,21 @@ void debounce(uint8_t bit)
 	_delay_ms(50);
 }
 
+void update_display()
+{
+	uint8_t temp = counter;
+	uint8_t tenths = 0;
+	
+	while(temp >= 10)
+	{
+		tenths++;
+		temp -= 10;
+	}
+	
+	digit1 = ~digits[tenths];
+	digit2 = ~digits[temp];
+}
+
 int main(void)
 {
 	setup();
@@ -59,20 +91,22 @@ int main(void)
 		
 		if (input != mask)
 		{
-			if ((input & _BV(PB1)) == 0)
+			if ((input & _BV(INC_BTN)) == 0)
 			{
 				if (counter < MAX)
 					counter++;
 				
-				debounce(PB1);
+				debounce(INC_BTN);
 			}
-			else if ((input & _BV(PB2)) == 0)
+			else if ((input & _BV(DEC_BTN)) == 0)
 			{
 				if (counter > MIN)
 					counter--;
 				
-				debounce(PB2);
+				debounce(DEC_BTN);
 			}
+			
+			update_display();
 		}
 	}
 	
